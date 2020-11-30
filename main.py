21eadea96ee8109d84ec4a3d404d4e76c524b304
ARLCROW <74873072+arlcrow123@users.noreply.github.com>
2020-11-30 14:30:18 +0300

    upd

---
+++ main.py
@@ -0,0 +1,10 @@
+# ARLCROW BARSIK
+# barsik.arlcrow.site
+# github.com/arlcrow123/barsik
+
+print("ARLCROW BARSIK")
+print("https://barsik.arlcrow.site")
+print("https://github.com/arlcrow123/barsik")
+print("---------")
+
+print("in the development")
